18ebba921b3d583c2d81c4a3f16f1fca5e800ba5
victorloo <ila9970@gmail.com>
2018-09-20 16:31:37 +0800

    Test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-if ENV["redis://rediscloud:q0fxpHBYYwPaLSqOiJdbIiedeuY2fGgR@redis-15172.c62.us-east-1-4.ec2.cloud.redislabs.com:15172"]
-  $redis = Redis.new(:url => ENV["redis://rediscloud:q0fxpHBYYwPaLSqOiJdbIiedeuY2fGgR@redis-15172.c62.us-east-1-4.ec2.cloud.redislabs.com:15172"])
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
