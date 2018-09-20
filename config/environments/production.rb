b3e2dee87e2f948f4a07fde27d60ae8ed91c37a2
victorloo <ila9970@gmail.com>
2018-09-20 16:15:07 +0800

    Test some changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://chatroom-with-heroku.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://chatroom-with-heroku.herokuapp.com/', 'https://chatroom-with-heroku.herokuapp.com/' ]
+  config.action_cable.allowe_request_origins = ['https://chatroom-with-heroku.herokuapp.com',
+                                                 'https://chatroom-with-heroku.herokuapp.com']
 
+  config.action_cable.url = 'wss://chatroom-with-heroku.herokuapp.com/cable'
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
